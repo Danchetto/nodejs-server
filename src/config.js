15ec1579d924ed5f7a05b00d6827912586b5a2fe
Daniil Tuchin <danchetto@yandex.ru>
2020-12-28 10:54:54 +0300

    Updated

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import DataBase from './DataBase/DataBase';
 const dataBase = new DataBase(
     {}, 
     {
-        user: process.env.DB_USER || 'tuchin',
-        database: process.env.DB_NAME || 'tuchinDB',
+        user: process.env.DB_USER || 'daniil.tuchin',
+        database: process.env.DB_NAME || 'tuchindb',
         password:  process.env.DB_PASSWORD || 'tuchin',
         host: '127.0.0.1',
         port: 5432
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import parser from 'koa-bodyparser';
 import logger from 'koa-logger';
 import router from './routes/routes';
 
-import sqlinjection from 'sql-injection';
+// import sqlinjection from 'sql-injection';
 
 const app = new Koa();
 const port = process.env.PORT || 5000;
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app
     .use(parser())
     .use(logger())
-    .use(sqlinjection)
+    // .use(sqlinjection)
     .use(router.routes());
 
 app.listen(port, () => console.log('Server is running on port:', port));
